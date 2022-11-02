011d7b06dadd94c8d36b7c89d849a43c72c9ea00
Muhammed <muhammed-gunaydin@hotmail.com>
2022-11-02 03:45:48 +0300

    added qr generator

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express')
+const path = require('path')
+const qrcode = require('qrcode')
 
 const app = express()
 
-const port = 3000
-app.listen(port, () => {
-  ;`Server running on port ${port}`
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
-app.use('/', (req, res) => {
-  res.json({ success: 'Node running' })
+app.post('/scan', (req, res, next) => {
+  const input_text = req.body.text
+  qrcode.toDataURL(input_text, (err, src) => {
+    if (err) res.send('Something went wrong!!')
+    res.render('scan', {
+      qr_code: src,
+    })
+  })
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log('Started listening on port ' + port)
 })
